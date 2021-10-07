c839b34d1bebf166f90b4df7235654c1260f98c0
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-10-07 14:02:59 +0300

    ffg
    dfg

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,23 +10,36 @@ class Counter extends Component {
             count: 11,
             colectionName: 'My Books'
         }
+
+        this.resetCounter = this.resetCounter.bind(this);
+//tova e za rezet bez .bind(this);
     }
 
-   decrementCounter() {
+   decrementCounter(e) {
       // this.setState({count: this.state.count - 1})
        this.setState(prevState => ({count: prevState.count - 1}))
    }
-   incrementCounter() {
+
+   incrementCounter(e) {
       this.setState(prevState => ({count: prevState.count +1}))
    }
+
+   resetCounter() {
+       this.setState({count: 0})
+   }
+   
+
     render(){
         return (
            <div className="counter">
                <h3>{this.state.colectionName} Count</h3>
-               <button onClick={(e) => this.decrementCounter}>-</button>
+               <button onClick={(e) => this.decrementCounter(e)}>-</button>
                <span>{this.state.count}</span>
                {/* <button onClick={(e) => this.setState((oldState) => ({count: oldState.count + 1}))}>+</button> */}
                <button onClick={this.incrementCounter.bind(this)}>+</button>
+
+               <button onClick={this.resetCounter.bind(this)}>Reset</button>
+               <button onClick={this.resetCounter}>Reset</button>
 
            </div>
         );
