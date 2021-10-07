9d6b538be9b770c9d6d8fbe2781a6aa75c36c5fc
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-10-07 13:51:24 +0300

    gg
    sd

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -12,13 +12,21 @@ class Counter extends Component {
         }
     }
 
+   decrementCounter() {
+      // this.setState({count: this.state.count - 1})
+       this.setState(prevState => ({count: prevState.count - 1}))
+   }
+   incrementCounter() {
+      this.setState(prevState => ({count: prevState.count +1}))
+   }
     render(){
         return (
            <div className="counter">
                <h3>{this.state.colectionName} Count</h3>
-               <button onClick={(e) => this.setState((oldState) => ({count: oldState.count - 1}))}>-</button>
+               <button onClick={(e) => this.decrementCounter}>-</button>
                <span>{this.state.count}</span>
-               <button onClick={(e) => this.setState((oldState) => ({count: oldState.count + 1}))}>+</button>
+               {/* <button onClick={(e) => this.setState((oldState) => ({count: oldState.count + 1}))}>+</button> */}
+               <button onClick={this.incrementCounter.bind(this)}>+</button>
 
            </div>
         );
