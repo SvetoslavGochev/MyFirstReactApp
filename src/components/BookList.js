2fed001aff892f4da64bd648e6c1cc51636fb721
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-10-14 15:14:17 +0300

    gg
    gg

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,35 +1,51 @@
 import { Component } from "react";
-
+import bookServices from '../services/bookServices'
 import Book from "./Book";
 
 class BookList extends Component {
-     constructor(props) {
-       super(props)
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      books: [],
+    };
+    //  kogato dannite idvat ot van6en sarvar
   }
 
-
-
   bookClicked(title) {
-     console.log("The book "+ title + "has been added to library")
+    console.log("The book " + title + "has been added to library");
+  }
+
+  componentDidMount() {
+    bookServices.getAll()
+    .then((books) => {
+      //knigite koito polu4avame  vtori promis
+      this.setState(() => ({ books })) //vra6ta obekt ot knigi
+    });
   }
 
   render() {
+
+// if(this.state.books.length == 0 ) {
+//   return <span>Loading books...</span>
+// }
+
     return (
       <div className="Book-List">
         <h4>Book Colection</h4>
-
-        {this.props.books.map((x) => 
-          < Book 
-             key={x.id}
-               title={x.title}
-               description={x.description}
-               //clickHandler={this.bookClicked.bind(this, x.title)} 
-                clickHandler={() => this.bookClicked(x.title)} 
-               author={x.author}
-                //!!!arow funkc darji konteksta na parenta ...nqma sobstven kontext!!!
+        {/* this.state.books.map((x) =>  */}
+        {this.props.books.map((x) =>  (
+          <Book
+            key={x.id}
+            title={x.title}
+            description={x.description}
+            //clickHandler={this.bookClicked.bind(this, x.title)}
+            clickHandler={() => this.bookClicked(x.title)}
+            author={x.author}
+            //!!!arow funkc darji konteksta na parenta ...nqma sobstven kontext!!!
           />
-        )}
-        
+        ))}
+
         {/* // <Book title={this.props.books[0].title} description="Wizard Book"/>
         // <Book title="Harry Poter2" description="Wizard Book2"/>
         // <Book title="Harry Poter3" description="Wizard Book3"/> */}
---
+++ src/services/bookServices.js
@@ -0,0 +1,9 @@
+function getAll() {
+  return fetch("sdsadsadasd") //vzzemi knigite ot tozi adres
+    .then((res) => res.json()) //responsa go napravi na json vra6ta promis
+    .catch(error => console.log(error))
+}
+
+export default {
+    getAll,
+}
